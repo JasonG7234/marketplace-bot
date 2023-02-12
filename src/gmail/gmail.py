94a0e651d6c945f74dee9df5ba9ae5ee9682c585
JasonG7234 <JasonG7234@gmail.com>
2023-02-12 10:53:37 -0500

    feat(gmail): emails sending

--- src/gmail/gmail.py
+++ src/gmail/gmail.py
@@ -1,16 +1,17 @@
 import base64
 from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from requests import HTTPError
 
 FLOW = InstalledAppFlow.from_client_secrets_file(
-            'credentials.json', [ "https://www.googleapis.com/auth/gmail.send" ])
+            'src/gmail/credentials.json', [ "https://www.googleapis.com/auth/gmail.send" ])
 creds = FLOW.run_local_server(port=8081)
 
-def send_email(data):
+def send_email(listings):
     service = build('gmail', 'v1', credentials=creds)
-    message = MIMEText(data)
+    message = MIMEText(populate_email_body(listings), 'html')
     message['to'] = 'JasonG7234@gmail.com'
     message['subject'] = 'Facebook Marketplace Deals'
     create_message = {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
@@ -20,26 +21,9 @@ def send_email(data):
     except HTTPError as error:
         print(F'An error occurred: {error}')
         message = None
-'''
-	SUBJECT = "BearDown Stats HTML"
-	FROMADDR = "variousemaillists@gmail.com"
-	FROMPASSWORD = "***" 
-	TOADDR = ['JasonG7234@gmail.com']
-	
-	MESSAGE = MIMEMultipart('alternative')
-	MESSAGE['subject'] = SUBJECT
-	MESSAGE['From'] = FROMADDR
 
-	HTML_BODY = MIMEText(data, 'html') #Record MIME type text/html
-	MESSAGE.attach(HTML_BODY)
-	
-	server = smtplib.SMTP('smtp.gmail.com:587')
-	server.starttls()
-	server.login(FROMADDR, FROMPASSWORD)
-	
-	for email in TOADDR:
-		MESSAGE['To'] = email
-		server.sendmail(FROMADDR, [email], MESSAGE.as_string())
-		
-	server.quit()
-'''
+from jinja2 import Template
+
+def populate_email_body(listings):
+	template = Template(open("src/gmail/email.html").read())
+	return template.render(listings=listings)
