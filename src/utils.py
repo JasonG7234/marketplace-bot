ed2c1055075859832381dd1977dda13bc8aa649d
JasonG7234 <JasonG7234@gmail.com>
2023-02-12 10:49:10 -0500

    chore(requests): Update user-agent

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import time
 API_URL = "https://www.facebook.com/api/graphql/"
 HEADERS = {
     "sec-fetch-site": "same-origin",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+    "user-agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
 }
 
 def post_request(payload, url=API_URL, headers=HEADERS, max_retry_count=3):
