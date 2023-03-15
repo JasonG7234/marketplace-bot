1bff0b021b42eb8672314fa9d063da7c05472c37
JasonG7234 <JasonG7234@gmail.com>
2023-03-14 21:04:59 -0400

    fix: send email with app password

--- src/gmail/gmail.py
+++ src/gmail/gmail.py
@@ -1,27 +1,27 @@
-import base64
 from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from requests import HTTPError
+import smtplib
+import json
 
-FLOW = InstalledAppFlow.from_client_secrets_file(
-            'src/gmail/credentials.json', 
-            [ "https://www.googleapis.com/auth/gmail.send" ])
-creds = FLOW.run_console()
-
-def send_email(listings):
-    service = build('gmail', 'v1', credentials=creds)
-    message = MIMEText(populate_email_body(listings), 'html')
-    message['to'] = 'JasonG7234@gmail.com'
-    message['subject'] = 'Facebook Marketplace Deals'
-    create_message = {'raw': base64.urlsafe_b64encode(message.as_bytes()).decode()}
-    try:
-        message = (service.users().messages().send(userId="me", body=create_message).execute())
-        print(F'sent message to {message} Message Id: {message["id"]}')
-    except HTTPError as error:
-        print(F'An error occurred: {error}')
-        message = None
+def send_email_old(listings):
+	SUBJECT = "Best Facebook Marketplace Listings"
+	FROMADDR = "variousemaillists@gmail.com"
+	FROMPASSWORD = json.load(open('src/gmail/app_password.json'))['app_password']
+	TOADDR = "JasonG7234@gmail.com"
+	
+	MESSAGE = MIMEMultipart('alternative')
+	MESSAGE['subject'] = SUBJECT
+	MESSAGE['From'] = FROMADDR
+	HTML_BODY = MIMEText(populate_email_body(listings), 'html')
+	MESSAGE.attach(HTML_BODY)
+	
+	server = smtplib.SMTP('smtp.gmail.com:587')
+	server.starttls()
+	server.login(FROMADDR, FROMPASSWORD)
+	
+	server.sendmail(FROMADDR, TOADDR, MESSAGE.as_string())
+		
+	server.quit()
 
 from jinja2 import Template
 
